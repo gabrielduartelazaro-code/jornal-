2f2a8560a60695cc2b0536d746abe01587287b3b
Gabriel Lazaro <gabriel.lazaro.2025133@my.istec.pt>
2026-04-15 00:41:51 +0100

    Adição dos modelos

--- jornal/urls.py
+++ jornal/urls.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # 👈 ESTA LINHA É A CHAVE
     path('', include('noticias.urls')),
 ]
